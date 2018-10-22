d915c17d78a8c7a44b5765247f5f984d0ede38c5
Lain <ehecattl@hotmail.com>
2018-10-21 19:52:09 -0500

    Terminado :D

--- app/src/main/java/com/example/eheca/lyricsapp/LyricsApp.kt
+++ app/src/main/java/com/example/eheca/lyricsapp/LyricsApp.kt
@@ -2,7 +2,6 @@ package com.example.eheca.lyricsapp
 
 import android.app.Application
 import android.content.Context
-import android.content.SharedPreferences
 import android.support.v7.app.AppCompatDelegate
 
 class LyricsApp: Application() {
